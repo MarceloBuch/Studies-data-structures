5622c63fd6b7a34e47585ba4ded7af74160e9ee6
MARCELO BUCHALOWICZ <marcelo.buchalowicz@etec.sp.gov.br>
2022-05-11 14:15:25 -0300

    added post-order traversal and compute tree height functions

--- arvore/tree.py
+++ arvore/tree.py
@@ -14,7 +14,8 @@ class BinaryTree:
             self.root = node
         else:
             self.root = None
-            
+
+    #percurso em ordem simétrica        
     def simetric_traversal(self, node=None):
         if node is None:
             node=self.root
@@ -23,6 +24,28 @@ class BinaryTree:
         print(node, end='')
         if node.right:
             self.simetric_traversal(node.right)
-        
 
+    #percurso em pós-ordem
+    def postorder_traversal(self, node=None):
+        if node is None:
+            node = self.root
+        if node.left:
+            self.postorder_traversal(node.left)
+        if node.right:
+            self.postorder_traversal(node.right)
+        print(node)
+
+    #altura da árvore
+    def height(self, node=None):
+        if node is None:
+            node = self.root
+        hleft = 0 
+        hright = 0
+        if node.left:
+            hleft = self.height(node.left)
+        if node.right:
+            hright = self.height(node.right)
+        if hright > hleft:
+            return hright + 1
+        return hleft + 1
     
